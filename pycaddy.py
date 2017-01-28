1b1f36d045208b7cdd1d511ecb40fc5fcff30ee7
Adrian L Lange <mail@p3lim.net>
2017-01-28 21:59:32 +0100

    Clean up the code a bit for readability

--- pycaddy.py
+++ pycaddy.py
@@ -1,5 +1,5 @@
 import ntpath
-import time, calendar
+import time
 import hmac, hashlib, base64
 import string, random
 
@@ -7,23 +7,21 @@ from .requests import requests
 
 class SignatureAuth(requests.auth.AuthBase):
 	def __init__(self, key, secret):
-		self.AUTH_HEADER = 'Signature keyId="{0}",algorithm="hmac-sha256",headers="timestamp token",signature="{1}"'
+		self.auth_header = 'Signature keyId="%s",signature="%%s"' % key
+		self.secret = secret
 
-		self.key = key
-		self.secret = secret.encode('utf-8')
-		self.token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32)).encode('utf-8')
+	def __call__(self, req):
+		timestamp = str(int(time.time()))
+		token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
 
-	def __call__(self, request):
-		timestamp = str(calendar.timegm(time.gmtime())).encode('utf-8')
-
-		digest = hmac.new(self.secret, timestamp + self.token, hashlib.sha256).digest()
+		digest = hmac.new(self.secret.encode('utf-8'), (timestamp + token).encode('utf-8'), hashlib.sha256).digest()
 		signature = base64.b64encode(digest).decode('utf-8')
 
-		request.headers['Token'] = self.token
-		request.headers['Timestamp'] = timestamp
-		request.headers['Authorization'] = self.AUTH_HEADER.format(self.key, signature)
+		req.headers['Token'] = token
+		req.headers['Timestamp'] = timestamp
+		req.headers['Authorization'] = self.auth_header % signature
 
-		return request
+		return req
 
 def upload(url, file_path, key=None, secret=None):
 	file_name = ntpath.basename(file_path)
@@ -32,10 +30,10 @@ def upload(url, file_path, key=None, secret=None):
 		files = {str(file_name): file}
 
 		if key and secret:
-			response = requests.post(url, files=files, auth=SignatureAuth(key, secret))
+			res = requests.post(url, files=files, auth=SignatureAuth(key, secret))
 		else:
-			response = requests.post(url, files=files)
+			res = requests.post(url, files=files)
 
-	response.raise_for_status()
+	res.raise_for_status()
 
-	return response.url + file_name
+	return res.url + file_name
